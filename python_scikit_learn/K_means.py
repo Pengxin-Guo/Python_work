69a940e15cb55bdf327968fa9140fe30b6a58a48
gpx <1457495424@qq.com>
2018-09-07 21:29:10 +0800

    modify K_means.py

--- python_scikit_learn/K_means.py
+++ python_scikit_learn/K_means.py
@@ -64,13 +64,13 @@ print('Start Kmeans:')
 from sklearn.cluster import KMeans
 ########################################################################
 #这儿为改进部分，利用肘部法则确定最优K值
-# """
+"""
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 font = FontProperties(fname=r"/usr/share/fonts/opentype/noto/NotoSerifCJK-Regular.ttc", size=10)  #deepin中的字体
-# font = FontProperties(fname=r"C:\Windows\Fonts\STSONG.TTF", size=10)                              #Windows中的字体
-from scipy.spatial.distance import cdist
-K=range(1,10)
+font = FontProperties(fname=r"C:\Windows\Fonts\STSONG.TTF", size=10)                              #Windows中的字体
+# from scipy.spatial.distance import cdist
+K=range(1,10)s
 meandistortions=[]
 for k in K:
     print("k=%d 聚类ing"%(k))
@@ -82,7 +82,7 @@ plt.xlabel('k')
 plt.ylabel(u'平均畸变程度', fontproperties=font)
 plt.title(u'用肘部法则来确定最佳的K值', fontproperties=font)
 plt.show()
-# """
+"""
 
 
 ########################################################################
